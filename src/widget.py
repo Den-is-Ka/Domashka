1c7a559b2491cbfb2e191d73b79abebe34772d7b
user <d_k81@mail.ru>
2025-06-08 21:18:45 +0500

    commit before handing in homework

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
 
+
 def mask_account_card(account_card: str) -> str:
     """
     Обрабатывает информацию о картах и счетах, и выводит маскировку.
