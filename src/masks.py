6d032267c7e3d66d86b94e26d09f719b0176e843
user <d_k81@mail.ru>
2025-06-08 20:59:29 +0500

    finish commit

--- src/masks.py
+++ src/masks.py
@@ -2,7 +2,7 @@ def get_mask_card_number(card_number: str) -> str:
     """
     Маскируем номер карты, оставляем последние 4 цифры.
     """
-    mask_card_number = card_number[:14] + ' ' + card_number[9:9] + '*******' + card_number[-4:]
+    mask_card_number = card_number[:4] + ' ' + card_number[4:6] + '*******' + card_number[-4:]
     return mask_card_number
 
 
@@ -10,11 +10,11 @@ def get_mask_account(account_number: str) -> str:
     """
     Маскируем номер счета.
     """
-    mask_account = account_number[:4] + ' ' + '**' + account_number[-4:]
+    mask_account = '**' + account_number[-4:]
     return mask_account
 
 
 if __name__ == '__main__':
-    print(get_mask_card_number('Visa Platinum 7000792289606361'))
+    print(get_mask_card_number('7000792289606361'))
 
-    print(get_mask_account('Cчет 73654108430135874305'))
+    print(get_mask_account('73654108430135874305'))
--- src/widget.py
+++ src/widget.py
@@ -4,33 +4,29 @@ def mask_account_card(account_card: str) -> str:
     """
     Обрабатывает информацию о картах и счетах, и выводит маскировку.
     """
-    parts = account_card.split()
-    type_account_card = " ".join(parts[:-1])
-    number = parts[-1]
-    return account_card
-
     if "счет" in account_card.lower():
-        number_card = account_card[-10:]
-        masked_card = get_mask_account(number_card)
-        return f"Счет {masked_card}"
+        number_card = account_card[-20:]
+        mask_account = get_mask_account(number_card)
+        return f"Счет {mask_account}"
+
     else:
-        name_card = account_card[-6:]
-        masked = get_mask_card_number(name_card)
-        bank_name = account_card[:-6]
-        return f"{bank_name} {masked}"
+        name_card = account_card[-16:]
+        mask_account = get_mask_card_number(name_card)
+        bank_name = account_card[:-16]
+    return f"{bank_name}{mask_account}"
 
 
-def get_date(data_number: str) -> str:
+def get_date(data_num: str) -> str:
     '''
-    Вывести дату в формате "ДД.ММ.ГГГГ"
+       Вывести дату в формате "ДД.ММ.ГГГГ"
     '''
-    correct = data_number[8:10] + "." + data_number[5:7] + "." + data_number[:4]
+    correct = data_num[8:10] + "." + data_num[5:7] + "." + data_num[:4]
     return correct
 
 
 if __name__ == '__main__':
-    print(get_mask_card_number('Visa Platinum 7000792289606361'))
+    print(mask_account_card('Visa Platinum 7000792289606361'))
 
-    print(get_mask_account('Cчет 73654108430135874305'))
+    print(mask_account_card('Счет 73654108430135874305'))
 
     print(get_date("1981-11-17T00:00:01.136347"))
