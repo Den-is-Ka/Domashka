e19b3d210b0e608eeda6d808145d1f40c5ff34f8
user <d_k81@mail.ru>
2025-06-29 21:14:55 +0500

    commit on homework 11_2_ dekorator bez testov

---
+++ src/decorators.py
@@ -0,0 +1,30 @@
+from functools import wraps
+
+from config import ROOT_DIR
+
+
+def log(filename=None):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwarks):
+            # Логируем старт
+            message = ""
+            try:
+                func(*args, **kwarks) # вызов исходника
+                message = f"Стартуем функцию {func.__name__} завершилась."
+            except Exception as e:
+                message = f"{func.__name__} тип ошибки {type(e)} с аргументами {args}, {kwarks}"
+            finally:
+                if filename:
+                    with open(f"{ROOT_DIR}/logs/{filename}", "a", encoding="utf-8") as f:
+                        f.write(message + "\n")
+                else:
+                    print(message)
+
+        return wrapper
+    return decorator
+
+@log(filename="Сюда пиши да.txt")
+@log()
+def hello():
+    print("Hi!")
