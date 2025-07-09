d67971e9f700f674b3860fd7e5e44167d4ebd5de
Den-is-Ka <D_K81@mail.ru>
2025-07-09 12:01:10 +0500

    коммит и пуш 12.1 чтобы сдать ДЗ

---
+++ src/utils.py
@@ -0,0 +1,26 @@
+import json
+
+from src.external_api import get_exchange_rate
+
+
+def load_transactions_json(file_path):
+
+    try:
+        with open(file_path, 'r', encoding='utf-8') as f:
+            return json.load(f)
+
+    except (json.JSONDecodeError, FileNotFoundError):
+        return []
+
+
+def convert_transaction_rub(transaction):
+    amount = transaction.get('operationAmount').get('amount')
+    currency = transaction.get('operationAmount').get('currency').get("code")
+
+    if currency == 'RUB':
+        return amount
+    elif currency in ['USD', 'EUR']:
+        result = get_exchange_rate(currency, amount)
+        return result
+    else:
+        return f'Неизвестная валюта: {currency}'
